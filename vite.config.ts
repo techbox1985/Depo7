45a3d9a6cb6f9409089ab81233723cdd6770399a
techbox1985 <techbox.argentina@gmail.com>
2026-03-20 23:17:28 -0300

    feat: Add maskable icon support to PWA
    Adds `purpose: 'any maskable'` to PWA manifest icons in vite.config.ts, allowing for dynamic icon display on various platforms.

--- vite.config.ts
+++ vite.config.ts
@@ -25,12 +25,14 @@ export default defineConfig(({mode}) => {
             {
               src: 'https://cdn.vectorstock.com/i/500p/98/75/shw-logo-design-template-with-strong-and-modern-vector-50999875.jpg',
               sizes: '192x192',
-              type: 'image/jpeg'
+              type: 'image/jpeg',
+              purpose: 'any maskable'
             },
             {
               src: 'https://cdn.vectorstock.com/i/500p/98/75/shw-logo-design-template-with-strong-and-modern-vector-50999875.jpg',
               sizes: '512x512',
-              type: 'image/jpeg'
+              type: 'image/jpeg',
+              purpose: 'any maskable'
             }
           ]
         },
